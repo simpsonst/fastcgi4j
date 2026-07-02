5951740a64e19549ad27fcb1c66fa250d959098d
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2026-07-02 19:36:21 +0100

    No need to declare unthrown exception.

--- src/java/augctxt/uk/ac/lancs/fastcgi/misc/SessionAugment.java
+++ src/java/augctxt/uk/ac/lancs/fastcgi/misc/SessionAugment.java
@@ -371,7 +371,7 @@ public final class SessionAugment {
      * 
      * @throws IllegalStateException if {@link #out()} has been called
      */
-    public void offerCompression() throws IOException {
+    public void offerCompression() {
         if (out != null)
             throw new IllegalStateException("too late to add compression");
         if (compressed) return;
