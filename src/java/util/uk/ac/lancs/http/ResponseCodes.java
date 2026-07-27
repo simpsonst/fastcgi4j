9022d59415209ecefdbf6174d366a8230ed2a126
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2026-07-27 07:54:33 +0100

    Revised, corrected, extended response-code symbols.

--- src/java/util/uk/ac/lancs/http/ResponseCodes.java
+++ src/java/util/uk/ac/lancs/http/ResponseCodes.java
@@ -49,7 +49,7 @@ public final class ResponseCodes {
      * Indicates that a request has been served successfully.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-200-ok">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.200">RFC9110
      * &mdash; 200 OK</a>
      */
     public static final int OK = 200;
@@ -58,7 +58,7 @@ public final class ResponseCodes {
      * Indicates that a resource was created.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-201-created">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.201">RFC9110
      * &mdash; 201 Created</a>
      */
     public static final int CREATED = 201;
@@ -67,7 +67,7 @@ public final class ResponseCodes {
      * Indicates that a request has been accepted for processing.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-202-accepted">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.202">RFC9110
      * &mdash; 202 Accepted</a>
      */
     public static final int ACCEPTED = 202;
@@ -77,7 +77,7 @@ public final class ResponseCodes {
      * not traverse away from its current document view.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-204-no-content">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.204">RFC9110
      * &mdash; 204 No Content</a>
      */
     public static final int NO_CONTENT = 204;
@@ -87,7 +87,7 @@ public final class ResponseCodes {
      * resource, by selecting from information in the response.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-300-multiple-choices">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.300">RFC9110
      * &mdash; 300 Multiple Choices</a>
      */
     public static final int MULTIPLE_CHOICES = 300;
@@ -97,7 +97,7 @@ public final class ResponseCodes {
      * another.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-301-moved-permanently">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.301">RFC9110
      * &mdash; 301 Moved Permanently</a>
      */
     public static final int MOVED_PERMANENTLY = 301;
@@ -107,7 +107,7 @@ public final class ResponseCodes {
      * request.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-302-found">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.302">RFC9110
      * &mdash; 302 Found</a>
      */
     public static final int FOUND = 302;
@@ -118,7 +118,7 @@ public final class ResponseCodes {
      * original request used a different method.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-303-see-other">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.303">RFC9110
      * &mdash; 303 See Other</a>
      */
     public static final int SEE_OTHER = 303;
@@ -128,7 +128,7 @@ public final class ResponseCodes {
      * cache.
      * 
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-304-not-modified">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.304">RFC9110
      * &mdash; 304 Not Modified</a>
      */
     public static final int NOT_MODIFIED = 304;
@@ -138,7 +138,7 @@ public final class ResponseCodes {
      * different URI, but that the current URI remains valid.
      * 
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-307-temporary-redirect">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.307">RFC9110
      * &mdash; 307 Temporary Redirect</a>
      */
     public static final int TEMPORARY_REDIRECT = 307;
@@ -148,7 +148,7 @@ public final class ResponseCodes {
      * different URI, and that the current URI should no longer be used.
      * 
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-308-permanent-redirect">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.308">RFC9110
      * &mdash; 308 Permanent Redirect</a>
      */
     public static final int PERMANENT_REDIRECT = 308;
@@ -157,7 +157,7 @@ public final class ResponseCodes {
      * Indicates that the client seems to have made a bad request.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-400-bad-request">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.400">RFC9110
      * &mdash; 400 Bad Request</a>
      */
     public static final int BAD_REQUEST = 400;
@@ -167,7 +167,7 @@ public final class ResponseCodes {
      * again with new credentials.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-401-unauthorized">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.401">RFC9110
      * &mdash; 401 Unauthorized</a>
      */
     public static final int UNAUTHORIZED = 401;
@@ -176,7 +176,7 @@ public final class ResponseCodes {
      * Indicates that the URI does not refer to a known resource.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-404-not-found">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.404">RFC9110
      * &mdash; 404 Not Found</a>
      */
     public static final int NOT_FOUND = 404;
@@ -185,7 +185,7 @@ public final class ResponseCodes {
      * Indicates that the request method is forbidden on the resource.
      *
      * @see <a href=
-     * "https://www.rfc-editor.org/rfc/rfc9110.html#name-405-method-not-allowed">RFC9110
+     * "https://www.rfc-editor.org/rfc/rfc9110.html#status.405">RFC9110
      * &mdash; 405 Method Not Allowed</a>
      */
     public static final int METHOD_NOT_ALLOWED = 405;
