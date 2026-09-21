49cfbb227c10da8e4b66497c5bc7dfa013c87d85
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2026-09-21 15:16:38 +0100

    More field name categories.

--- src/java/util/uk/ac/lancs/http/field/FieldNameSets.java
+++ src/java/util/uk/ac/lancs/http/field/FieldNameSets.java
@@ -38,6 +38,7 @@
 
 package uk.ac.lancs.http.field;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
@@ -50,66 +51,7 @@ import static uk.ac.lancs.http.field.FieldNames.*;
  * @author simpsons
  */
 public class FieldNameSets {
-    /**
-     * An immutable case-insensitive set of entity end-to-end field
-     * names
-     */
-    public static final Set<String> ENTITY_END_TO_END =
-        nameSet(ALLOW, CONTENT_BASE, CONTENT_ENCODING, CONTENT_LANGUAGE,
-                CONTENT_LOCATION, CONTENT_MD5, CONTENT_RANGE, CONTENT_TYPE,
-                ETAG, EXPIRES, LAST_MODIFIED);
-
-    /**
-     * An immutable case-insensitive set of general end-to-end field
-     * names
-     */
-    public static final Set<String> GENERAL_END_TO_END =
-        Set.of(CACHE_CONTROL, DATE, PRAGMA, UPGRADE, VIA, MAN, OPT);
-
-    /**
-     * An immutable case-insensitive set of request hop-by-hop field
-     * names
-     */
-    public static final Set<String> REQUEST_HOP_BY_HOP =
-        nameSet(MAX_FORWARDS, PROXY_AUTHORIZATION, TE);
-
-    /**
-     * An immutable case-insensitive set of response end-to-end field
-     * names
-     */
-    public static final Set<String> RESPONSE_END_TO_END =
-        nameSet(AGE, LOCATION, RETRY_AFTER, SERVER, VARY, WARNING,
-                WWW_AUTHENTICATE, EXT, SET_COOKIE);
-
-    /**
-     * An immutable case-insensitive set of request end-to-end field
-     * names
-     */
-    public static final Set<String> REQUEST_END_TO_END =
-        nameSet(ACCEPT, ACCEPT_CHARSET, ACCEPT_ENCODING, ACCEPT_LANGUAGE,
-                AUTHORIZATION, FROM, HOST, IF_UNMODIFIED_SINCE, IF_MATCH,
-                IF_NONE_MATCH, IF_RANGE, IF_MODIFIED_SINCE, RANGE, REFERER,
-                USER_AGENT, COOKIE);
-
-    /**
-     * An immutable case-insensitive set of general hop-by-hop field
-     * names
-     */
-    public static final Set<String> GENERAL_HOP_BY_HOP =
-        nameSet(CONNECTION, TRANSFER_ENCODING, C_MAN, C_OPT, TRAILER);
-
-    /**
-     * An immutable case-insensitive set of response hop-by-hop field
-     * names
-     */
-    public static final Set<String> RESPONSE_HOP_BY_HOP =
-        nameSet(PROXY_AUTHENTICATE, PUBLIC, C_EXT);
-
-    /**
-     * An immutable case-insensitive set of entity hop-by-hop field
-     * names
-     */
-    public static final Set<String> ENTITY_HOP_BY_HOP = nameSet(CONTENT_LENGTH);
+    private FieldNameSets() {}
 
     /**
      * Create an immutable set of case-insensitive strings.
@@ -119,10 +61,255 @@ public class FieldNameSets {
      * @return the requested set
      */
     static Set<String> nameSet(CharSequence... names) {
-        return Collections.unmodifiableSet(nameSet(names).stream()
-            .map(Object::toString).collect(Collectors
-                .toCollection(() -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER))));
+        return Collections
+            .unmodifiableSet(nameSet(names).stream().map(Object::toString)
+                .collect(Collectors.toCollection(FieldNameSets::newSet)));
     }
 
-    private FieldNameSets() {}
+    /**
+     * Create an immutable set of case-insensitive strings from other
+     * collections.
+     * 
+     * @param parts the other sets
+     * 
+     * @return the union of the supplied sets
+     */
+    static Set<String> nameSet(Collection<Collection<String>> parts) {
+        return Collections
+            .unmodifiableSet(parts.stream().flatMap(e -> e.stream())
+                .collect(Collectors.toCollection(FieldNameSets::newSet)));
+    }
+
+    /**
+     * An immutable case-insensitive set of entity end-to-end field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_ENTITY_END_TO_END =
+        nameSet(CONTENT_ENCODING, CONTENT_RANGE, CONTENT_TYPE);
+
+    /**
+     * An immutable case-insensitive set of entity end-to-end field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_ENTITY_END_TO_END =
+        nameSet(ALLOW, CONTENT_BASE, CONTENT_LANGUAGE, CONTENT_LOCATION,
+                CONTENT_MD5, ETAG, EXPIRES, LAST_MODIFIED);
+
+    /**
+     * An immutable case-insensitive set of entity end-to-end field
+     * names
+     */
+    public static final Set<String> ENTITY_END_TO_END =
+        nameSet(Set.of(HEADER_ENTITY_END_TO_END, CAP_ENTITY_END_TO_END));
+
+    /**
+     * An immutable case-insensitive set of general end-to-end field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_GENERAL_END_TO_END =
+        Set.of(CACHE_CONTROL, MAN, OPT, DATE, PRAGMA, UPGRADE, VIA);
+
+    /**
+     * An immutable case-insensitive set of general end-to-end field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_GENERAL_END_TO_END = Set.of();
+
+    /**
+     * An immutable case-insensitive set of general end-to-end field
+     * names
+     */
+    public static final Set<String> GENERAL_END_TO_END =
+        nameSet(Set.of(HEADER_GENERAL_END_TO_END, CAP_GENERAL_END_TO_END));
+
+    /**
+     * An immutable case-insensitive set of request hop-by-hop field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_REQUEST_HOP_BY_HOP =
+        nameSet(MAX_FORWARDS, PROXY_AUTHORIZATION, TE);
+
+    /**
+     * An immutable case-insensitive set of request hop-by-hop field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_REQUEST_HOP_BY_HOP = nameSet();
+
+    /**
+     * An immutable case-insensitive set of request hop-by-hop field
+     * names
+     */
+    public static final Set<String> REQUEST_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_REQUEST_HOP_BY_HOP, CAP_REQUEST_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of response end-to-end field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_RESPONSE_END_TO_END =
+        nameSet(LOCATION, RETRY_AFTER, SERVER, VARY, WWW_AUTHENTICATE, EXT,
+                SET_COOKIE);
+
+    /**
+     * An immutable case-insensitive set of response end-to-end field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_RESPONSE_END_TO_END =
+        nameSet(AGE, WARNING);
+
+    /**
+     * An immutable case-insensitive set of response end-to-end field
+     * names
+     */
+    public static final Set<String> RESPONSE_END_TO_END =
+        nameSet(Set.of(HEADER_RESPONSE_END_TO_END, CAP_RESPONSE_END_TO_END));
+
+    /**
+     * An immutable case-insensitive set of request end-to-end field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_REQUEST_END_TO_END =
+        nameSet(ACCEPT, ACCEPT_CHARSET, ACCEPT_ENCODING, ACCEPT_LANGUAGE,
+                AUTHORIZATION, FROM, HOST, IF_UNMODIFIED_SINCE, IF_MATCH,
+                IF_NONE_MATCH, IF_RANGE, IF_MODIFIED_SINCE, RANGE, REFERER,
+                USER_AGENT, COOKIE);
+
+    /**
+     * An immutable case-insensitive set of request end-to-end field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_REQUEST_END_TO_END = nameSet();
+
+    /**
+     * An immutable case-insensitive set of request end-to-end field
+     * names
+     */
+    public static final Set<String> REQUEST_END_TO_END =
+        nameSet(Set.of(HEADER_REQUEST_END_TO_END, CAP_REQUEST_END_TO_END));
+
+    /**
+     * An immutable case-insensitive set of general hop-by-hop field
+     * names that may only appear in a header, and that must be declared
+     * in a <samp>{@value "%s" FieldNames#CONNECTION}</samp> field
+     */
+    public static final Set<String> HEADER_GENERAL_EXPLICIT_HOP_BY_HOP =
+        nameSet(C_MAN, C_OPT);
+
+    /**
+     * An immutable case-insensitive set of general hop-by-hop field
+     * names that may only appear in a header, and that need not be
+     * declared in a <samp>{@value "%s" FieldNames#CONNECTION}</samp>
+     * field
+     */
+    public static final Set<String> HEADER_GENERAL_IMPLICIT_HOP_BY_HOP =
+        nameSet(CONNECTION, TRANSFER_ENCODING, TRAILER);
+
+    /**
+     * An immutable case-insensitive set of general hop-by-hop field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_GENERAL_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_GENERAL_EXPLICIT_HOP_BY_HOP,
+                       HEADER_GENERAL_IMPLICIT_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of general hop-by-hop field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_GENERAL_HOP_BY_HOP = nameSet();
+
+    /**
+     * An immutable case-insensitive set of general hop-by-hop field
+     * names
+     */
+    public static final Set<String> GENERAL_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_GENERAL_HOP_BY_HOP, CAP_GENERAL_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of response hop-by-hop field
+     * names that may only appear in a header, and that must be declared
+     * in a <samp>{@value "%s" FieldNames#CONNECTION}</samp> field
+     */
+    public static final Set<String> HEADER_RESPONSE_EXPLICIT_HOP_BY_HOP =
+        nameSet(C_EXT);
+
+    /**
+     * An immutable case-insensitive set of response hop-by-hop field
+     * names that may only appear in a header, and that need not be
+     * declared in a <samp>{@value "%s" FieldNames#CONNECTION}</samp>
+     * field
+     */
+    public static final Set<String> HEADER_RESPONSE_IMPLICIT_HOP_BY_HOP =
+        nameSet(PROXY_AUTHENTICATE, PUBLIC);
+
+    /**
+     * An immutable case-insensitive set of response hop-by-hop field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_RESPONSE_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_RESPONSE_EXPLICIT_HOP_BY_HOP,
+                       HEADER_RESPONSE_IMPLICIT_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of response hop-by-hop field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_RESPONSE_HOP_BY_HOP = nameSet();
+
+    /**
+     * An immutable case-insensitive set of response hop-by-hop field
+     * names
+     */
+    public static final Set<String> RESPONSE_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_RESPONSE_HOP_BY_HOP, CAP_RESPONSE_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of entity hop-by-hop field
+     * names that may only appear in a header
+     */
+    public static final Set<String> HEADER_ENTITY_HOP_BY_HOP =
+        nameSet(CONTENT_LENGTH);
+
+    /**
+     * An immutable case-insensitive set of entity hop-by-hop field
+     * names that may appear in either header or trailer
+     */
+    public static final Set<String> CAP_ENTITY_HOP_BY_HOP = nameSet();
+
+    /**
+     * An immutable case-insensitive set of entity hop-by-hop field
+     * names
+     */
+    public static final Set<String> ENTITY_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_ENTITY_HOP_BY_HOP, CAP_ENTITY_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of hop-by-hop field names that
+     * need not be declared in a <samp>{@value "%s"
+     * FieldNames#CONNECTION}</samp> field
+     */
+    public static final Set<String> IMPLICIT_HOP_BY_HOP =
+        nameSet(Set.of(HEADER_GENERAL_IMPLICIT_HOP_BY_HOP,
+                       HEADER_RESPONSE_IMPLICIT_HOP_BY_HOP));
+
+    private static TreeSet<String> newSet() {
+        return new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+    }
+
+    /**
+     * An immutable case-insensitive set of hop-by-hop field names
+     */
+    public static final Set<String> HOP_BY_HOP =
+        nameSet(Set.of(GENERAL_HOP_BY_HOP, ENTITY_HOP_BY_HOP,
+                       REQUEST_HOP_BY_HOP, RESPONSE_HOP_BY_HOP));
+
+    /**
+     * An immutable case-insensitive set of field names that may only
+     * appear in a header
+     */
+    public static final Set<String> HEADER =
+        nameSet(Set.of(HEADER_ENTITY_END_TO_END, HEADER_ENTITY_HOP_BY_HOP,
+                       HEADER_GENERAL_END_TO_END, HEADER_GENERAL_HOP_BY_HOP,
+                       HEADER_REQUEST_END_TO_END, HEADER_REQUEST_HOP_BY_HOP,
+                       HEADER_RESPONSE_END_TO_END, HEADER_RESPONSE_HOP_BY_HOP));
 }
